2ad9d9810261ef86346c70381a390275169f49c1
doaaaliahmed <doaaali3@yahoo.com>
2024-01-01 12:38:09 +0200

    comment send date func

--- app/page.tsx
+++ app/page.tsx
@@ -20,18 +20,18 @@ import Loader from "./components/Loader";
 const Home = () => {
   const [events, setEvents] = useState<IEvent[]>();
 
-  const saveNotes = async () => {
-    const events = doc(database, "events", "eventList");
-    await updateDoc(events, {
-      ev: arrayUnion({
-        title: "Music world Tour",
-        desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved.",
-        location: "Germany, Berlin",
-        date: Timestamp.fromDate(new Date("August 19, 2024 9:00")),
-        speaker: "Lysa sandiago",
-      }),
-    });
-  };
+  // const saveNotes = async () => {
+  //   const events = doc(database, "events", "eventList");
+  //   await updateDoc(events, {
+  //     ev: arrayUnion({
+  //       title: "Music world Tour",
+  //       desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved.",
+  //       location: "Germany, Berlin",
+  //       date: Timestamp.fromDate(new Date("August 19, 2024 9:00")),
+  //       speaker: "Lysa sandiago",
+  //     }),
+  //   });
+  // };
 
   useEffect(() => {
     const getEvent = async () => {
@@ -58,7 +58,7 @@ const Home = () => {
           <Hero firstEvent={events.at(0)!} />
           <Speakers />
           <Events events={events!} />
-          <button onClick={saveNotes}>AddEvent</button>
+          {/* <button onClick={saveNotes}>AddEvent</button> */}
         </main>
       )}
     </>
